f096d9e2546dfc36ad7fbe24a8e067004107bcda
Aller Augusto de Souza <aller.souza@samsung.com>
2019-01-16 17:49:02 -0200

    Retrive info from main activity and set image

--- app/src/main/java/com/hudeing/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/hudeing/caraoucoroa/ResultadoActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
     private Button buttonVoltar;
+    private ImageView imageResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +17,25 @@ public class ResultadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
 
         buttonVoltar = findViewById(R.id.buttonVoltar);
+        imageResultado = findViewById(R.id.imageResultado);
+
+        //Recuperar dados
+        Bundle bundle = getIntent().getExtras();
+        int numero = bundle.getInt("numero");
+
+        if (numero == 0) {
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        } else {
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        //Evento de clique para o botao
         buttonVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                /*Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);*/
+                finish();
             }
         });
     }
