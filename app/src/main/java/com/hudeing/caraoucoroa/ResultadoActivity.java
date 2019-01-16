65401ea3c1c224e99cdfd803e357532a91836a26
Aller Augusto de Souza <aller.souza@samsung.com>
2019-01-16 17:18:52 -0200

    Add initial structure

---
+++ app/src/main/java/com/hudeing/caraoucoroa/ResultadoActivity.java
@@ -0,0 +1,26 @@
+package com.hudeing.caraoucoroa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ResultadoActivity extends AppCompatActivity {
+    private Button buttonVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
